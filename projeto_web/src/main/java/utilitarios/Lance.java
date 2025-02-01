c701a1c9adfbab1cbecb94a1a0e0c5c2144b42d8
Matteo Savan <matteosavan@gmail.com>
2025-02-01 16:40:00 -0300

    classe Lance addicionada

---
+++ projeto_web/src/main/java/utilitarios/Lance.java
@@ -0,0 +1,42 @@
+package utilitarios;
+
+
+public class Lance {
+    String nome_produto;
+    double valor;
+    int id_produto;
+
+    // Construtor
+    public Lance(String nome_produto, double valor, int id_produto) {
+        this.nome_produto = nome_produto;
+        this.valor = valor;
+        this.id_produto = id_produto;
+    }
+
+    // Getter e Setter para nome_produto
+    public String get_nome_produto() {
+        return nome_produto;
+    }
+
+    public void set_nome_produto(String nome_produto) {
+        this.nome_produto = nome_produto;
+    }
+
+    // Getter e Setter para valor
+    public double get_valor() {
+        return valor;
+    }
+
+    public void set_valor(double valor) {
+        this.valor = valor;
+    }
+
+    // Getter e Setter para id_produto
+    public int get_id() {
+        return id_produto;
+    }
+
+    public void set_id(int id_produto) {
+        this.id_produto = id_produto;
+    }
+}
